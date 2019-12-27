4b4aebe57d78093d06a03dce539a13af5e05b69c
Nickid2018 on School Computer <Administrator@SKY-20190520KRP>
2019-12-27 13:03:57 +0800

    Change the style of rendering

--- Chemistry/src/com/chemistrylab/layer/component/VerticalSlideBar.java
+++ Chemistry/src/com/chemistrylab/layer/component/VerticalSlideBar.java
@@ -35,31 +35,40 @@ public class VerticalSlideBar extends Component {
 	public void debugRender() {
 		float mysize = range.y1 - range.y0;
 		float shouldDraw = cons.size() * vertsize;
-		float percent = mysize / shouldDraw;
-		if (percent > 1)
-			percent = 0;
-		float barlength = (percent == 0 ? 1 : percent) * mysize;
-		float drawup = (mysize - barlength) * postion + range.y0;
-		float drawdown = (mysize - barlength) * postion + barlength + range.y0;
-		slibar.y0 = (int) drawup;
-		slibar.y1 = (int) drawdown;
-		// Bar
-		barcolor.bind();
-		glBegin(GL_QUADS);
-			glVertex2f(range.x1 - barheight, drawup);
-			glVertex2f(range.x1 - barheight, drawdown);
-			glVertex2f(range.x1, drawdown);
-			glVertex2f(range.x1, drawup);
-		glEnd();
-		// Inside
-		int canDraws = MathHelper.fastFloor(mysize / vertsize);
-		int firstnear = MathHelper.fastFloor(postion * cons.size());
-		int first = firstnear - 1;
-		for (int i = first < 0 ? 0 : first, count = 0; i < cons.size() && i < firstnear + canDraws; i++, count++) {
-			Slidable s = cons.get(i);
-			s.setNowPositon(range.x0, range.x1, MathHelper.fastFloor(mysize * count / canDraws + range.y0),
-					MathHelper.fastFloor(mysize * count / canDraws + range.y0 + vertsize));
-			s.debugRender();
+		if (shouldDraw > mysize) {
+			float percent = mysize / shouldDraw;
+			if (percent > 1)
+				percent = 0;
+			float barlength = (percent == 0 ? 1 : percent) * mysize;
+			float drawup = (mysize - barlength) * postion + range.y0;
+			float drawdown = (mysize - barlength) * postion + barlength + range.y0;
+			slibar.y0 = (int) drawup;
+			slibar.y1 = (int) drawdown;
+			// Bar
+			barcolor.bind();
+			glBegin(GL_QUADS);
+				glVertex2f(range.x1 - barheight, drawup);
+				glVertex2f(range.x1 - barheight, drawdown);
+				glVertex2f(range.x1, drawdown);
+				glVertex2f(range.x1, drawup);
+			glEnd();
+			// Inside
+			int canDraws = MathHelper.fastFloor(mysize / vertsize);
+			int firstnear = MathHelper.fastFloor(postion * cons.size());
+			int first = firstnear - 1;
+			for (int i = first < 0 ? 0 : first, count = 0; i < cons.size() && i < firstnear + canDraws; i++, count++) {
+				Slidable s = cons.get(i);
+				s.setNowPositon(range.x0, range.x1, MathHelper.fastFloor(mysize * count / canDraws + range.y0),
+						MathHelper.fastFloor(mysize * count / canDraws + range.y0 + vertsize));
+				s.debugRender();
+			} 
+		} else {
+			for (int i = 0; i < cons.size() && i < cons.size(); i++) {
+				Slidable s = cons.get(i);
+				s.setNowPositon(range.x0, range.x1, MathHelper.fastFloor(i * vertsize + range.y0),
+						MathHelper.fastFloor(i * vertsize + range.y0 + vertsize));
+				s.debugRender();
+			} 
 		}
 	}
 
@@ -68,29 +77,40 @@ public class VerticalSlideBar extends Component {
 		super.render();
 		float mysize = range.y1 - range.y0;
 		float shouldDraw = cons.size() * vertsize;
-		float percent = mysize / shouldDraw;
-		if (percent > 1)
-			percent = 0;
-		float barlength = (percent == 0 ? 1 : percent) * mysize;
-		float drawup = (mysize - barlength) * postion + range.y0;
-		float drawdown = (mysize - barlength) * postion + barlength + range.y0;
-		// Bar
-		barcolor.bind();
-		glBegin(GL_QUADS);
-			glVertex2f(range.x1 - barheight, drawup);
-			glVertex2f(range.x1 - barheight, drawdown);
-			glVertex2f(range.x1, drawdown);
-			glVertex2f(range.x1, drawup);
-		glEnd();
-		// Inside
-		int canDraws = MathHelper.fastFloor(mysize / vertsize);
-		int firstnear = MathHelper.fastFloor(postion * cons.size());
-		int first = firstnear - 1;
-		for (int i = first < 0 ? 0 : first, count = 0; i < cons.size() && i < firstnear + canDraws - 1; i++, count++) {
-			Slidable s = cons.get(i);
-			s.setNowPositon(range.x0, range.x1 - barheight, MathHelper.fastFloor(mysize * count / canDraws + range.y0),
-					MathHelper.fastFloor(mysize * count / canDraws + range.y0 + vertsize));
-			s.render();
+		if (shouldDraw > mysize) {
+			float percent = mysize / shouldDraw;
+			if (percent > 1)
+				percent = 0;
+			float barlength = (percent == 0 ? 1 : percent) * mysize;
+			float drawup = (mysize - barlength) * postion + range.y0;
+			float drawdown = (mysize - barlength) * postion + barlength + range.y0;
+			slibar.y0 = (int) drawup;
+			slibar.y1 = (int) drawdown;
+			// Bar
+			barcolor.bind();
+			glBegin(GL_QUADS);
+				glVertex2f(range.x1 - barheight, drawup);
+				glVertex2f(range.x1 - barheight, drawdown);
+				glVertex2f(range.x1, drawdown);
+				glVertex2f(range.x1, drawup);
+			glEnd();
+			// Inside
+			int canDraws = MathHelper.fastFloor(mysize / vertsize);
+			int firstnear = MathHelper.fastFloor(postion * cons.size());
+			int first = firstnear - 1;
+			for (int i = first < 0 ? 0 : first, count = 0; i < cons.size() && i < firstnear + canDraws; i++, count++) {
+				Slidable s = cons.get(i);
+				s.setNowPositon(range.x0, range.x1, MathHelper.fastFloor(mysize * count / canDraws + range.y0),
+						MathHelper.fastFloor(mysize * count / canDraws + range.y0 + vertsize));
+				s.render();
+			} 
+		} else {
+			for (int i = 0; i < cons.size() && i < cons.size(); i++) {
+				Slidable s = cons.get(i);
+				s.setNowPositon(range.x0, range.x1, MathHelper.fastFloor(i * vertsize + range.y0),
+						MathHelper.fastFloor(i * vertsize + range.y0 + vertsize));
+				s.render();
+			} 
 		}
 	}
 
